1172147809dcf8ea949591671393d980e97309d8
karthik2214 <71806384+karthik2214@users.noreply.github.com>
2021-03-10 13:20:07 +0530

    Add files via upload

---
+++ src/index.js
@@ -0,0 +1,21 @@
+import React from "react";
+import ReactDOM from "react-dom";
+//import './index.css';
+
+ReactDOM.render(
+<React.Fragment>  <h1> My favorite Anime </h1>
+  <p> hii guys I am Zero </p>
+<ol>
+  <li> One piece</li>
+  <li> Attack on titan</li>
+  <li> hunter x hunter</li>
+  <li> Death note</li>
+  <li> Naruto</li>
+</ol>
+
+<h1>
+  hello
+</h1>
+  </React.Fragment> ,
+ document.getElementById('root'));
+// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
